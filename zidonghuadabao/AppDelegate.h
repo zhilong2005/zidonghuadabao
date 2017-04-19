748f82f1cdc4b6cb408b7a37e52f2e60da55bd0a
sumpay <sumpay@sumpays-mbp.lan>
2017-04-19 08:51:16 +0800

    Initial Commit

---
+++ zidonghuadabao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zidonghuadabao
+//
+//  Created by sumpay on 2017/4/19.
+//  Copyright © 2017年 sumpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zidonghuadabaoTests/zidonghuadabaoTests.m
@@ -0,0 +1,39 @@
+//
+//  zidonghuadabaoTests.m
+//  zidonghuadabaoTests
+//
+//  Created by sumpay on 2017/4/19.
+//  Copyright © 2017年 sumpay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface zidonghuadabaoTests : XCTestCase
+
+@end
+
+@implementation zidonghuadabaoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
